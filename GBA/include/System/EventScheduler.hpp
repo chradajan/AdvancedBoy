613491c5998565d03b63d0b8765fedfd5a1c1db6
chradajan <chradajan@gmail.com>
2024-07-08 23:32:37 -0700

    Implement event scheduler

--- GBA/include/System/EventScheduler.hpp
+++ GBA/include/System/EventScheduler.hpp
@@ -1,10 +1,72 @@
 #pragma once
 
+#include <functional>
+#include <unordered_map>
+#include <vector>
 #include <GBA/include/Types.hpp>
+
+/// @brief Enum of various event types that can be scheduled to execute. Must be registered before scheduling.
+enum class EventType
+{
+    // EventScheduler
+    VDraw,
+    HBlank,
+    VBlank,
+
+    // Total number of events. Do not schedule this, and do not place events below this.
+    COUNT
+};
+
+/// @brief Information about an event that is schedule to be executed.
+struct Event
+{
+    /// @brief Check which event executes sooner. If A > B, then A should execute after B. If two events execute on the same cycle,
+    ///        determine priority by their order in the EventType enum (lower = higher priority).
+    /// @param rhs Event to compare priority to.
+    /// @return True if this is lower priority than the event this is being compared to.
+    bool operator>(Event const& rhs);
+
+    EventType eventType_;
+    u64 cycleQueued_;
+    u64 cycleToExecute_;
+};
 
 /// @brief Scheduler for scheduling and dispatching system events.
 class EventScheduler
 {
+    using Callback = std::function<void(int)>;
+
 public:
+    EventScheduler(EventScheduler const&) = delete;
+    EventScheduler& operator=(EventScheduler const&) = delete;
+    EventScheduler(EventScheduler&&) = delete;
+    EventScheduler& operator=(EventScheduler&&) = delete;
+
+    /// @brief Initialize the event scheduler by preallocating space in the event queue.
+    EventScheduler();
+
+    /// @brief Register a callback function for an event. All callbacks should be registered during component initialization.
+    /// @param event Event to install callback for.
+    /// @param callback Function to call when event fires.
+    void RegisterEvent(EventType event, Callback callback);
+
+    /// @brief Schedule an event to fire in some number of cycles.
+    /// @param event Event type to schedule.
+    /// @param cycles Number of cycles from now to fire event.
+    void ScheduleEvent(EventType event, int cycles);
+
+    /// @brief Advance the scheduler by some number of cycles and execute any scheduled events that have occurred.
+    /// @param cycles Number of cycles to advance the scheduler by.
+    void Step(int cycles);
+
+    /// @brief Advance the scheduler to whenever the next scheduled event would occur.
+    void FireNextEvent();
+
 private:
+    /// @brief Execute any scheduled events which were scheduled to execute at or before the current total cycle count.
+    void CheckEventQueue();
+
+    std::vector<Event> queue_;
+    std::unordered_map<EventType, Callback> callbacks_;
+    u64 totalCycles_;
 };
--- GBA/src/System/EventScheduler.cpp
+++ GBA/src/System/EventScheduler.cpp
@@ -1,2 +1,71 @@
 #include <GBA/include/System/EventScheduler.hpp>
+#include <algorithm>
+#include <functional>
+#include <stdexcept>
+#include <unordered_map>
+#include <vector>
 #include <GBA/include/Types.hpp>
+
+bool Event::operator>(Event const& rhs)
+{
+    if (cycleToExecute_ == rhs.cycleToExecute_)
+    {
+        return eventType_ > rhs.eventType_;
+    }
+
+    return cycleToExecute_ > rhs.cycleToExecute_;
+}
+
+EventScheduler::EventScheduler()
+{
+    queue_.reserve(static_cast<size_t>(EventType::COUNT) + 1);
+    totalCycles_ = 0;
+}
+
+void EventScheduler::RegisterEvent(EventType event, Callback callback)
+{
+    if (callbacks_.contains(event))
+    {
+        throw std::logic_error("Duplicate event registration");
+    }
+
+    callbacks_.insert({event, callback});
+}
+
+void EventScheduler::ScheduleEvent(EventType event, int cycles)
+{
+    if (cycles < 0)
+    {
+        throw std::runtime_error("Illegal event scheduler timing");
+    }
+
+    u64 cycleToExecute = totalCycles_ + cycles;
+    queue_.push_back({event, totalCycles_, cycleToExecute});
+    std::make_heap(queue_.begin(), queue_.end(), std::greater<>{});
+}
+
+void EventScheduler::Step(int cycles)
+{
+    totalCycles_ += cycles;
+    CheckEventQueue();
+}
+
+void EventScheduler::FireNextEvent()
+{
+    totalCycles_ = queue_.front().cycleToExecute_;
+    CheckEventQueue();
+}
+
+void EventScheduler::CheckEventQueue()
+{
+    Event nextEvent = queue_.front();
+
+    while (totalCycles_ >= nextEvent.cycleToExecute_)
+    {
+        std::pop_heap(queue_.begin(), queue_.end(), std::greater<>{});
+        queue_.pop_back();
+        auto& callback = callbacks_[nextEvent.eventType_];
+        callback(totalCycles_ - nextEvent.cycleToExecute_);
+        nextEvent = queue_.front();
+    }
+}
